87edbba00f9363271c548ae6e878c6c78e13abeb
Rodrigo Romero <rodrigo.romero@vnstudios.com>
2016-07-12 13:00:24 -0300

    Add Sample DB

--- rr_db.php
+++ rr_db.php
@@ -1,5 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $db['default']['hostname'] = '127.0.0.1';
-$db['default']['username'] = 'adblicka_rrWeb';
-$db['default']['password'] = 'ev=X}[T^PSzh';
-$db['default']['database'] = 'adblicka_omgeventos';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '123456';
+$db['default']['database'] = 'omgeventos';
