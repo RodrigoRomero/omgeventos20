639d59a9682f2965fb8af54f7db42e2d6e97d508
Rodrigo Romero <rodrigo.romero@vnstudios.com>
2016-07-12 01:09:00 -0300

    Amdia Version

--- application/sistema/views/forms/iae.php
+++ application/sistema/views/forms/iae.php
@@ -2,9 +2,9 @@
 $data   = array ('id'=>'ticketsFormIAE', 'class'=>'form relative');
 $action = lang_url('cart/add');
 echo form_open($action,$data);
-echo form_hidden('ticket_sku','iae_0-2');
+echo form_hidden('ticket_sku','amdia_0-1');
 echo form_hidden('ticket_ammount',0);
-echo form_hidden('ticket_name','Miembro Antiguo IAE');
+echo form_hidden('ticket_name','Socio AMDIA');
 
     
 echo '<div class="col-md-12 text-center">';
@@ -16,8 +16,8 @@ echo form_close();
 ?>
 <script>
 function resetValues(){
-    $('input[name=ticket_sku').val('iae_0-2');
+    $('input[name=ticket_sku').val('amdia_0-1');
     $('input[name=ticket_ammount').val(0);
-    $('input[name=ticket_name').val('Miembro Antiguo IAE');
+    $('input[name=ticket_name').val('Socio AMDIA');
 }
 </script>
