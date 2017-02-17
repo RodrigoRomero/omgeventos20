02ce88ee1f2411bcace027bd38baa3dc41840cc4
Rodrigo Romero <rodrigo.romero@vnstudios.com>
2017-02-17 11:29:53 -0300

    profile customer

---
+++ application/sistema/views/accounts/index.php
@@ -0,0 +1,16 @@
+<section >
+	<div class="container">
+		<header class="text-center">
+			<h1>My Store</h1>
+		</header>
+		<p>Acá deberíamos levantar</p>
+        <ul>
+        	
+        	
+        	<li>1 - my account, con los datos de la emrepsa / customer</li>
+        	<li>2 - levantar el listado de todos los eventos que participo</li>
+        	<li>2.1 - levantar todas las entradas que compró</li>
+        	<li>2.1.1 - poder nominar / renovminar las entradas</li>
+        </ul>
+	</div>
+</section>
